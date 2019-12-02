a551489343cc0137c0148f75601e83d7d1be20c8
talpat <talonpatterson94@gmail.com>
2019-12-02 10:10:00 +0200

    knex setup

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,62 +1,90 @@
-import { Server } from 'http'
-// @ts-ignore
-import createLogger, { Logger } from 'pino'
-import { createApp } from './app'
+import { Server } from "http";
+import createLogger from "pino";
+import { createApp } from "./app";
+import Knex = require("knex");
 
-const logger = createLogger()
-logger.level = process.env.LOG_LEVEL || 'info'
-const PORT = process.env.PORT || 3001
+const logger = createLogger();
+logger.level = process.env.LOG_LEVEL || "info";
+const PORT = process.env.PORT || 3001;
+const KNEX_CLIENT = process.env.KNEX_CLIENT || "sqlite3";
+
+const knex =
+  KNEX_CLIENT === "mysql"
+    ? Knex({
+        client: "mysql",
+        connection: {
+          host: process.env.MYSQL_HOST,
+          user: process.env.MYSQL_USER,
+          password: process.env.MYSQL_PASSWORD,
+          database: process.env.MYSQL_DATABASE
+        }
+      })
+    : Knex({
+        client: "sqlite3",
+        connection: {
+          filename: ":memory:"
+        }
+      });
 
 const app = createApp({
   logger
-})
+});
 
-let server: Server
+let server: Server;
 export const gracefulShutdown = async (): Promise<void> => {
-  logger.info('shutting down.')
+  logger.info("shutting down.");
   if (server) {
     return new Promise((resolve, reject): void => {
       server.close((err?: Error) => {
         if (err) {
-          reject(err)
-          return
+          reject(err);
+          return;
         }
-        resolve()
-      })
-    })
+        knex.destroy();
+        resolve();
+      });
+    });
   }
-}
+};
 
 export const start = async (): Promise<void> => {
-  let shuttingDown = false
-  process.on('SIGINT', async (): Promise<void> => {
-    try {
-      if (shuttingDown) {
-        logger.warn('received second SIGINT during graceful shutdown, exiting forcefully.')
-        process.exit(1)
-        return
+  let shuttingDown = false;
+  process.on(
+    "SIGINT",
+    async (): Promise<void> => {
+      try {
+        if (shuttingDown) {
+          logger.warn(
+            "received second SIGINT during graceful shutdown, exiting forcefully."
+          );
+          process.exit(1);
+          return;
+        }
+
+        shuttingDown = true;
+
+        // Graceful shutdown
+        await gracefulShutdown();
+        logger.info("completed graceful shutdown.");
+      } catch (err) {
+        const errInfo =
+          err && typeof err === "object" && err.stack ? err.stack : err;
+        logger.error("error while shutting down. error=%s", errInfo);
+        process.exit(1);
       }
-
-      shuttingDown = true
-
-      // Graceful shutdown
-      await gracefulShutdown()
-      logger.info('completed graceful shutdown.')
-    } catch (err) {
-      const errInfo = (err && typeof err === 'object' && err.stack) ? err.stack : err
-      logger.error('error while shutting down. error=%s', errInfo)
-      process.exit(1)
     }
-  })
+  );
 
-  server = app.listen(PORT)
-  logger.info(`Listening on ${PORT}`)
-}
+  await knex.migrate.latest();
+
+  server = app.listen(PORT);
+  logger.info(`Listening on ${PORT}`);
+};
 
 // If this script is run directly, start the server
 if (!module.parent) {
   start().catch(e => {
-    const errInfo = (e && typeof e === 'object' && e.stack) ? e.stack : e
-    logger.error(errInfo)
-  })
+    const errInfo = e && typeof e === "object" && e.stack ? e.stack : e;
+    logger.error(errInfo);
+  });
 }
