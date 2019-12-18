88cdaf23748906bb266fd4787fa652aff2e1de21
Cairin Michie <cairinmichie@gmail.com>
2019-12-18 11:47:15 +0200

    Fix login styling (#22)
    * Fix login styling
    * home screen colours to match new branding.

--- packages/frontend/pages/login.tsx
+++ packages/frontend/pages/login.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { NextPage } from "next"
 import useForm from 'react-hook-form'
 import { UsersService } from '../services/users'
+import PhoneInput, { isValidPhoneNumber, isPossiblePhoneNumber } from 'react-phone-number-input'
+import './signup/phone-input.css'
+import Input from '../components/input'
+import FormButton from '../components/form-button'
 
 const usersService = UsersService()
 
@@ -11,64 +15,65 @@ type Props = {
   login_challenge: string
 }
 
+type FormData = {
+  phoneNumber: string
+  password: string
+}
+
 const Login: NextPage<Props> = ({login_challenge}) => {
-  const {register, handleSubmit, errors, setError} = useForm()
+  const { register, setValue, handleSubmit, errors, setError } = useForm<FormData>()
 
-
-  const onSubmit = async data => {
-    const login = await usersService.login(data.username, data.password, login_challenge).then(resp => {
-      if(resp.redirectTo) {
-        window.location.href = resp.redirectTo
-      }
-    }).catch(error => {
-      console.log(error)
-    })
+  const onSubmit = handleSubmit(async (props) => {
+    if (!isValidPhoneNumber(props.phoneNumber)) {
+      setError('phoneNumber', 'Invalid phone number.', '')
+    } else {
+      await usersService.login(props.phoneNumber, props.password, login_challenge).then(resp => {
+        if(resp.redirectTo) {
+          window.location.href = resp.redirectTo
+        }
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  })
+  const onValueChange = (phoneNumber) => {
+    if (!phoneNumber) {
+      setError('phoneNumber', 'Phone number required.', '')
+    } else if (!isValidPhoneNumber(phoneNumber)) {
+      setError('phoneNumber', 'Invalid phone number.', '')
+    } else {
+      setValue('phoneNumber', phoneNumber, true)
+    }
   }
 
   return (
-    <div className="w-full max-w-xs mx-auto md:mt-32">
-      <div className="flex flex-col mb-6">
-        <img className="mx-auto h-32" src={'/Logo.svg'}/>
-        <div className="text-center text-gray-800 text-lg font-light">
-          Welcome Back!
+    <div className="flex flex-wrap content-center items-center justify-center text-center w-full h-screen">
+      <div className="w-full text-gray-800 mb-10 text-headline">Welcome back!</div>
+      <form className="w-3/4 max-w-sm" onSubmit={onSubmit}>
+        <div className="w-full">
+          <input type="hidden" name="phoneNumber" ref={register({ required: true })}/>
+          <PhoneInput
+            smartCaret={false}
+            placeholder="Phone number"
+            name={'phoneNumber'}
+            inputClassName={'appearance-none bg-gray-100 border-b border-light focus:border-primary py-2 px-3 mb-3 w-full leading-tight focus:outline-none'}
+            onChange={onValueChange}
+            error={errors.phoneNumber ? errors.phoneNumber.type : ''}
+          />
         </div>
-      </div>
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-3">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Username</label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            name="username" ref={(register({required: true}))}
-            autoComplete="username"
-            id="username" type="text" placeholder="Username"/>
-          <p className="ml-1 h-2 text-xs text-red-600">{errors.username ? errors.username.message : ""}</p>
+        <div className="w-full">
+          <Input
+            type={'password'}
+            formRef={register({ required: true })}
+            name={'password'}
+            className={'appearance-none bg-gray-100 border-b border-light focus:border-primary w-full py-2 px-3 mb-3 leading-tight focus:outline-none'}
+            placeholder={'***********'}
+          />
         </div>
-        <div className="mb-6">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            name="password" ref={(register({required: true}))}
-            autoComplete="current-password"
-            id="password" type="password" placeholder="******************"/>
-          <p className="ml-1 h-2 text-xs text-red-600">{errors.password ? errors.password.message : ""}</p>
+        <div className="w-full">
+          <FormButton>Login</FormButton>
         </div>
-        <button
-          className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="submit">
-          Login
-        </button>
       </form>
-      <div className="text-center mt-2 text-gray-600 text-light text-sm">
-        Don't have an account?
-      </div>
-      <a href="/signup">
-        <button
-          className="shadow-md border-2 border-gray-500 text-gray-600 hover:bg-gray-500 hover:text-white w-full font-bold py-2 px-4 my-4 rounded"
-          type="button">
-          Create an Account
-        </button>
-      </a>
-      <p className="text-center text-gray-500 text-xs">©2019 Rafiki Money. All rights reserved.</p>
     </div>
   )
 }
