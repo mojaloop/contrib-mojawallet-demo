dc2e0fea6b9b01e9f229845c7d0332836188f0fa
Cairin Michie <cairinmichie@gmail.com>
2020-01-08 11:44:56 +0200

    Add profile page (#30)

--- packages/frontend/pages/profile.tsx
+++ packages/frontend/pages/profile.tsx
@@ -1,97 +1,24 @@
 import React from 'react'
-import Head from 'next/head'
-import { NextPage, NextPageContext } from 'next'
-import Link from 'next/link'
-import { ProfilePageProps } from "../types"
-import { formatCurrency, checkUser } from "../utils"
-import { AccountsService } from '../services/accounts'
+import { NextPage } from "next"
+import Button from '../components/button'
+import { checkUser } from '../utils'
+import { ProfilePageProps } from '../types'
 
-const accountsService = AccountsService()
-
-const Account: NextPage<ProfilePageProps> = ({ user }) => {
+const Signup: NextPage<ProfilePageProps> = ({user}) => {
   return (
-    <div>
-      <Head>
-        <title>Account</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="fixed top-0 right-0" style={{ zIndex:1 }}>
-        <Link href={{ pathname: '/' }}>
-          <div className="mr-5 mt-5">
-            <img style={{ height: '35px'}} src={'../../icons/close-24px.svg'}/>
-          </div>
-        </Link>
+    <div className="flex flex-wrap content-center items-center justify-center text-center w-full h-screen">
+      <img className="h-32" src={'/Logo.svg'}/>
+      <div className="w-full mt-5 text-gray-800 text-headline">{user.username}</div>
+      <div className="w-full mt-20">
+        <Button to="/logout" text={false}>logout</Button>
       </div>
-      {/* <div className='w-full fixed top-0 shadow-2xl' style={{textDecoration: 'none', color: 'inherit', height: '16rem', background: 'linear-gradient(#225980, #7caab2)', borderRadius: '0 0 20px 20px',zIndex:0 }}>
-        <div className='w-full mx-auto max-w-lg'>
-          
-          <div className="flex">
-          
-            <div className="text-3xl text-white flex-1 text-base mx-4 px-4 mt-20">
-              {account.name}
-            </div>
-          </div>
-          <div className="flex flex-wrap text-2xl text-white mx-10">
-            <div className="w-1/2">
-              Balance:
-            </div>
-            <div className="w-1/2 text-right">
-              {formatCurrency(account.balance, account.assetScale)}
-            </div>
-          </div>
-
-        </div>
-      </div>
-      <div className="w-full flex my-4 flex-wrap" style={{marginTop: '16rem'}}>
-        <div className="mt-4 text-xl px-6 py-4 mx-8">
-          Transactions
-        </div>
-        { accountList.length > 0 ? accountList.map(account => <AccountCard key={'account_' + account.id} account={account}/>) : 'No Accounts present.'}
-      </div> */}
     </div>
   )
 }
 
-export interface AccountDetails {
-  id: number
-  name: string
-  balance: number
-  owner: number,
-  assetScale: number
-}
+export default Signup
 
-type AccountCardProps = {
-  account: AccountDetails
-}
-
-const AccountCard: React.FC<AccountCardProps> = ({ account }) => {
-  return (
-    <Link href="/account/[account.id]"  as={`/account/${account.id}`}>
-      <div className="w-auto rounded-lg shadow-md flex flex-col w-full mt-8 px-6 py-4 mx-8" style={{textDecoration: 'none', color: 'inherit', background: 'white'}}>
-        <div className="flex flex-1">
-          <div className="flex-1">
-            <div className="text-3xl">
-              {formatCurrency(account.balance, 6)}
-            </div>
-            <div className="text-sm text-grey">
-              Balance
-            </div>
-          </div>
-          <div>
-            {/* <img src={'../../icons/close-24px-white.svg'}/> */}
-          </div>
-        </div>
-        <div className="text-grey-dark">
-          {account.name}
-        </div>
-      </div>
-    </Link>
-  )
-}
-
-export default Account
-
-Account.getInitialProps = async (ctx) => {
+Signup.getInitialProps = async (ctx) => {
   const user = await checkUser(ctx)
   return { user }
 }
