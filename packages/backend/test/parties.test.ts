66e5851ffe31539486eb6aaf471c2385ec3251e0
Cairin <cairinmichie@gmail.com>
2020-01-08 13:24:37 +0200

    Fix parties tests to use appContainer

--- packages/backend/test/parties.test.ts
+++ packages/backend/test/parties.test.ts
@@ -1,116 +1,35 @@
-import Koa from 'koa'
 import axios from 'axios'
-import { createApp } from '../src/app'
-import { Server } from 'http'
-import { KnexAccountService } from '../src/services/accounts-service'
-import { KnexTransactionService } from '../src/services/transactions-service'
-import { KnexUserService } from '../src/services/user-service'
-import { KnexTransactionRequestService } from '../src/services/transaction-request-service'
-import createLogger from 'pino'
-import { HydraApi, TokenInfo } from '../src/apis/hydra'
-import { KnexQuoteService } from '../src/services/quote-service'
-import { MojaloopRequests } from '@mojaloop/sdk-standard-components'
-import { KnexOtpService } from '../src/services/otp-service'
-import Knex = require('knex')
+import { createTestApp, TestAppContainer } from './utils/app'
 
 describe('Parties Request Test', () => {
-  let server: Server
-  let port: number
-  let app: Koa
-  let knex: Knex
-  let accountsService: KnexAccountService
-  let transactionsService: KnexTransactionService
-  let userService: KnexUserService
-  let transactionRequestService: KnexTransactionRequestService
-  let quoteService: KnexQuoteService
-  let otpService: KnexOtpService
-  let hydraApi: HydraApi
-  const mojaloopRequests = new MojaloopRequests({
-    dfspId: 'mojawallet',
-    jwsSign: false,
-    jwsSigningKey: 'test',
-    logger: console,
-    peerEndpoint: '',
-    alsEndpoint: '',
-    tls: { outbound: { mutualTLS: { enabled: false } } }
-  })
+  let appContainer: TestAppContainer
 
   beforeAll(async () => {
-    knex = Knex({
-      client: 'sqlite3',
-      connection: {
-        filename: ':memory:'
-      }
-    })
-    accountsService = new KnexAccountService(knex)
-    transactionsService = new KnexTransactionService(knex)
-    userService = new KnexUserService(knex)
-    transactionRequestService = new KnexTransactionRequestService(knex)
-    quoteService = new KnexQuoteService(knex)
-    otpService = new KnexOtpService(knex)
-    hydraApi = {
-      introspectToken: async (token) => {
-        if (token === 'user1token') {
-          return {
-            sub: '1',
-            active: true
-          } as TokenInfo
-        } else if (token === 'user2token') {
-          return {
-            sub: '2',
-            active: true
-          } as TokenInfo
-        } else if (token === 'user3token') {
-          return {
-            sub: '3',
-            active: false
-          } as TokenInfo
-        } else {
-          throw new Error('Getting Token failed')
-        }
-      }
-    } as HydraApi
-
-    app = createApp({
-      knex,
-      accountsService,
-      transactionsService,
-      transactionRequestService,
-      logger: createLogger(),
-      hydraApi,
-      userService,
-      quoteService,
-      mojaloopRequests,
-      otpService
-    })
-    server = app.listen(0)
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-    // @ts-ignore
-    port = server.address().port
+    appContainer = createTestApp()
   })
 
   beforeEach(async () => {
-    await knex.migrate.latest()
-    await userService.store({
+    await appContainer.knex.migrate.latest()
+    await appContainer.userService.store({
       username: '+27123456789',
       password: 'password'
     })
   })
 
   afterEach(async () => {
-    await knex.migrate.rollback()
+    await appContainer.knex.migrate.rollback()
   })
 
   afterAll(() => {
-    server.close()
-    knex.destroy()
+    appContainer.server.close()
+    appContainer.knex.destroy()
   })
 
   describe('Handling a parties get request', () => {
     test('Can retrieve a valid parties request and returns 200', async () => {
       const mock = jest.fn()
-      mojaloopRequests.putParties = mock
-      const response = await axios.get(`http://localhost:${port}/parties/msisdn/27123456789`, {
+      appContainer.mojaloopRequests.putParties = mock
+      const response = await axios.get(`http://localhost:${appContainer.port}/parties/msisdn/27123456789`, {
         headers: {
           'fspiop-source': 'MobileMoney',
           'fspiop-destination': 'mojawallet'
