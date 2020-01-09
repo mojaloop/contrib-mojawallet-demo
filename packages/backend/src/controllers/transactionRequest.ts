bf531d32759145480e88a9160f3bbcffc3589edc
Cairin <cairinmichie@gmail.com>
2020-01-09 10:54:52 +0200

    Elimination testing

--- packages/backend/src/controllers/transactionRequest.ts
+++ packages/backend/src/controllers/transactionRequest.ts
@@ -1,11 +1,12 @@
 import { AccountsAppContext } from '../index'
-import { QuoteTools } from '../services/quote-service'
-import { mojaResponseService } from '../services/mojaResponseService'
+// import { QuoteTools } from '../services/quote-service'
+// import { mojaResponseService } from '../services/mojaResponseService'
 import { TransactionRequestsPostRequest } from '../types/mojaloop'
 
 export async function create (ctx: AccountsAppContext): Promise<void> {
   ctx.logger.info('Create transaction request called')
-  const { transactionRequests, quotes, users } = ctx
+  const { users } = ctx
+  // const { transactionRequests, quotes, users } = ctx
   const { body } = ctx.request
   const payerUserName = (body as TransactionRequestsPostRequest).payer.partyIdentifier
 
@@ -14,34 +15,34 @@ export async function create (ctx: AccountsAppContext): Promise<void> {
   const user = await users.getByUsername(payerUserName)
   ctx.logger.info(user, 'transactionRequests user')
 
-  try {
-    const response = await transactionRequests.create(body, user.id)
-    ctx.logger.info(response, 'transactionRequests called')
+  // try {
+  //   const response = await transactionRequests.create(body, user.id)
+  //   ctx.logger.info(response, 'transactionRequests called')
 
-    // potentially change to a queing system for asynchronous responses to avoid unhandled promises
-    mojaResponseService.putResponse(
-      {
-        transactionRequestState: 'RECEIVED'
-      },
-      body.transactionRequestId
-    )
-    ctx.status = 200
+  //   // potentially change to a queing system for asynchronous responses to avoid unhandled promises
+  //   mojaResponseService.putResponse(
+  //     {
+  //       transactionRequestState: 'RECEIVED'
+  //     },
+  //     body.transactionRequestId
+  //   )
+  //   ctx.status = 200
 
-    const quoteTools = new QuoteTools(body)
-    await quotes.add(quoteTools.getQuote())
-    mojaResponseService.quoteResponse(quoteTools.getQuote())
-  } catch (error) {
-    ctx.logger.error(error, 'Error in transactionRequests')
-    mojaResponseService.putErrorResponse(
-      {
-        errorInformation: {
-          errorCode: '3100',
-          errorDescription: 'Invalid transaction request',
-          extensionList: []
-        }
-      },
-      body.transactionRequestId
-    )
-    ctx.status = 400
-  }
+  //   const quoteTools = new QuoteTools(body)
+  //   await quotes.add(quoteTools.getQuote())
+  //   mojaResponseService.quoteResponse(quoteTools.getQuote())
+  // } catch (error) {
+  //   ctx.logger.error(error, 'Error in transactionRequests')
+  //   mojaResponseService.putErrorResponse(
+  //     {
+  //       errorInformation: {
+  //         errorCode: '3100',
+  //         errorDescription: 'Invalid transaction request',
+  //         extensionList: []
+  //       }
+  //     },
+  //     body.transactionRequestId
+  //   )
+  //   ctx.status = 400
+  // }
 }
