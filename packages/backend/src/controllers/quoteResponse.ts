145cf9d89136e9d13479533a31ebb205227cf8c7
Cairin <cairinmichie@gmail.com>
2020-01-13 10:24:25 +0200

    Edit quote response

--- packages/backend/src/controllers/quoteResponse.ts
+++ packages/backend/src/controllers/quoteResponse.ts
@@ -5,22 +5,17 @@ export async function quoteResponse (ctx: AccountsAppContext): Promise<void> {
   const { quotes } = ctx
   const { id } = ctx.params
   const { body } = ctx.request
-  const retrievedQuote = await quotes.get(id)
-
-  if (!retrievedQuote) {
-    ctx.status = 404
-  } else {
-    try {
-      const quoteResponseTools = new QuoteResponseTool(body, id)
-      await quotes.update(id, {
-        quoteResponse: quoteResponseTools.getSerializedResponse()
-      })
-      quoteResponseTools.initAuthorization()
-      ctx.status = 200
-      return
-    } catch (error) {
-      console.log(error)
-      ctx.status = 400
-    }
+  // TODO: Fire off an error if can't find quote.
+  // const retrievedQuote = await quotes.get(id)
+  ctx.status = 200
+  try {
+    const quoteResponseTools = new QuoteResponseTool(body, id)
+    await quotes.update(id, {
+      quoteResponse: quoteResponseTools.getSerializedResponse()
+    })
+    quoteResponseTools.initAuthorization()
+    return
+  } catch (error) {
+    console.log(error)
   }
 }
