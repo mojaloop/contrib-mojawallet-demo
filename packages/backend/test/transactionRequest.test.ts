371cfcf6fba8ad636af9eb5b72528c68a392d229
Talon Patterson <38129947+TalPat@users.noreply.github.com>
2020-01-16 13:32:16 +0200

    transaction request error responses (#81)

--- packages/backend/test/transactionRequest.test.ts
+++ packages/backend/test/transactionRequest.test.ts
@@ -2,6 +2,8 @@ import axios from 'axios'
 import { mojaResponseService } from '../src/services/mojaResponseService'
 import { createTestApp, TestAppContainer } from './utils/app'
 import { TransactionRequestsPostRequest } from '../src/types/mojaloop'
+import { cloneDeep } from 'lodash'
+
 jest.mock('../src/services/mojaResponseService', () => ({
   mojaResponseService: {
     putResponse: jest.fn(),
@@ -70,6 +72,21 @@ describe('Transaction Request Test', () => {
       username: '+27123456789',
       password: 'password'
     })
+    await appContainer.accountsService.add({
+      userId: '1',
+      name: '+27123456789',
+      assetCode: 'USD',
+      assetScale: 2,
+      limit: 0n
+    })
+    await appContainer.otpService.add({
+      userId: '1',
+      accountId: '1',
+      expiresAt: Date.now() / 1000 + 300,
+      isUsed: false,
+      otp: '1111'
+    })
+    await appContainer.transactionsService.create('1', BigInt(10000), 'Test Money')
   })
 
   afterEach(async () => {
@@ -102,22 +119,105 @@ describe('Transaction Request Test', () => {
       }
     })
 
-    test('An invalid transaction request does not store data and returns 400', async () => {
-      await axios.post(`http://localhost:${appContainer.port}/transactionRequests`, invalidRequest, {
-        headers: { 'Content-Type': 'application/vnd.interoperability.transactionRequests+json;version=1.0', 'FSPIOP-Source': 'mojawallet' }
+    test('A transaction request with a non-existant party should return an error', async () => {
+      let badPartyRequest: TransactionRequestsPostRequest = cloneDeep(validRequest)
+      badPartyRequest.payer.partyIdentifier = '27321321321'
+      const response = await axios.post(
+        `http://localhost:${appContainer.port}/transactionRequests`,
+        badPartyRequest,
+        { headers: { 'Content-Type': 'application/vnd.interoperability.transactionRequests+json;version=1.0', 'FSPIOP-Source': 'mojawallet' }
       }).catch(async error => {
-        const storedRequest = await appContainer.transactionRequestService.getByRequestId(invalidRequest.transactionRequestId)
-        expect(storedRequest).toBeUndefined()
-        expect(error.response.status).toEqual(400)
-        expect(mojaResponseService.putErrorResponse).toHaveBeenCalledWith({
-          errorInformation: {
-            errorCode: '3100',
-            errorDescription: 'Invalid transaction request',
-            extensionList: []
-          }
-        }, invalidRequest.transactionRequestId,
-        'mojawallet')
+        expect(error).toBeUndefined()
       })
+      const storedRequest = await appContainer.transactionRequestService.getByRequestId(invalidRequest.transactionRequestId)
+      expect(storedRequest).toBeUndefined()
+      if (response)
+        expect(response.status).toEqual(202)
+      expect(response).toBeTruthy()
+      expect(mojaResponseService.putErrorResponse).toHaveBeenCalledWith({
+        errorInformation: {
+          errorCode: '3204',
+          errorDescription: 'Party not found',
+          extensionList: []
+        }
+      }, validRequest.transactionRequestId,
+      'mojawallet')
+    })
+
+    test('A transaction request in which the payer has insufficient funds should return an error', async () => {
+      await appContainer.transactionsService.create('1', BigInt(-10000), 'Remove Money')
+      const response = await axios.post(
+        `http://localhost:${appContainer.port}/transactionRequests`,
+        validRequest,
+        { headers: { 'Content-Type': 'application/vnd.interoperability.transactionRequests+json;version=1.0', 'FSPIOP-Source': 'mojawallet' }
+      }).catch(async error => {
+        expect(error).toBeUndefined()
+      })
+      const storedRequest = await appContainer.transactionRequestService.getByRequestId(invalidRequest.transactionRequestId)
+      expect(storedRequest).toBeUndefined()
+      if (response)
+        expect(response.status).toEqual(202)
+      expect(response).toBeTruthy()
+      expect(mojaResponseService.putErrorResponse).toHaveBeenCalledWith({
+        errorInformation: {
+          errorCode: '4000',
+          errorDescription: 'Payer error, unable to complete transaction request',
+          extensionList: []
+        }
+      }, validRequest.transactionRequestId,
+      'mojawallet')
+    })
+
+    test('A transaction request in which the payer has no active otp should return an error', async () => {
+      const otpProps = {
+        userId: '1',
+        isUsed: false,
+        expiresAt: Date.now() / 1000
+      }
+      await appContainer.otpService.update(otpProps)
+      const response = await axios.post(
+        `http://localhost:${appContainer.port}/transactionRequests`,
+        validRequest,
+        { headers: { 'Content-Type': 'application/vnd.interoperability.transactionRequests+json;version=1.0', 'FSPIOP-Source': 'mojawallet' }
+      }).catch(async error => {
+        expect(error).toBeUndefined()
+      })
+      const storedRequest = await appContainer.transactionRequestService.getByRequestId(invalidRequest.transactionRequestId)
+      expect(storedRequest).toBeUndefined()
+      if (response)
+        expect(response.status).toEqual(202)
+      expect(response).toBeTruthy()
+      expect(mojaResponseService.putErrorResponse).toHaveBeenCalledWith({
+        errorInformation: {
+          errorCode: '4000',
+          errorDescription: 'Payer error, unable to complete transaction request',
+          extensionList: []
+        }
+      }, validRequest.transactionRequestId,
+      'mojawallet')
+    })
+
+    test('An invalid transaction request does not store data and returns an error', async () => {
+      const response = await axios.post(
+        `http://localhost:${appContainer.port}/transactionRequests`,
+        invalidRequest,
+        { headers: { 'Content-Type': 'application/vnd.interoperability.transactionRequests+json;version=1.0', 'FSPIOP-Source': 'mojawallet' }
+      }).catch(async error => {
+        expect(error).toBeUndefined()
+      })
+      const storedRequest = await appContainer.transactionRequestService.getByRequestId(invalidRequest.transactionRequestId)
+      expect(storedRequest).toBeUndefined()
+      if (response)
+        expect(response.status).toEqual(202)
+      expect(response).toBeTruthy()
+      expect(mojaResponseService.putErrorResponse).toHaveBeenCalledWith({
+        errorInformation: {
+          errorCode: '3100',
+          errorDescription: 'Invalid transaction request',
+          extensionList: []
+        }
+      }, invalidRequest.transactionRequestId,
+      'mojawallet')
     })
   })
 })
