5bcaa2cbb15a2d6a0d771297ed57816f2446c81b
donchangfoot <donovanchangfoot@gmail.com>
2020-09-07 13:02:29 +0200

    chore(backend): add codecov thresholds

--- packages/backend/jest.config.js
+++ packages/backend/jest.config.js
@@ -1,4 +1,22 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    modulePathIgnorePatterns: ["dist"],
+    coverageThreshold: {
+        global: {
+          statements: 80,
+          functions: 80,
+          branches: 70,
+          lines: 80
+        }
+      },
+      collectCoverageFrom: [
+        "src/**/*.ts",
+        "!src/index.ts",
+        "!src/apis/hydra.ts",
+        "!src/controllers/oauth2Client.ts",
+        "!src/controllers/refreshALS.ts",
+        "!src/controllers/logout.ts",
+        "!src/controllers/consent.ts"
+      ]
 };
